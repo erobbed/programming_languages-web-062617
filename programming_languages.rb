b8bdfe35af3f26821ee0386fa1a44fd82ef79336
Ethan Roberts <ethan.reid.roberts@gmail.com>
2017-06-13 15:13:53 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,52 @@
+require "pry"
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+
+  languages.each do |style, language_data|
+    case style
+    when :oo
+      language_data.each do |language_name, attributes|
+        case language_name
+        when :ruby
+          attributes.each do |type, type_value|
+            new_hash[:ruby] = {type => type_value, :style => [style]}
+          end
+        when :javascript
+          attributes.each do |type, type_value|
+            new_hash[:javascript] = {type => type_value, :style => [style]}
+          end
+        when :python
+          attributes.each do |type, type_value|
+            new_hash[:python] = {type => type_value, :style => [style]}
+          end
+        when :java
+          attributes.each do |type, type_value|
+            new_hash[:java] = {type => type_value, :style => [style]}
+          end
+        end
+      end
+    when :functional
+      language_data.each do |language_name, attributes|
+        case language_name
+        when :clojure
+          attributes.each do |type, type_value|
+            new_hash[:clojure] = {type => type_value, :style => [style]}
+          end
+        when :erlang
+          attributes.each do |type, type_value|
+            new_hash[:erlang] = {type => type_value, :style => [style]}
+          end
+        when :scala
+          attributes.each do |type, type_value|
+            new_hash[:scala] = {type => type_value, :style => [style]}
+          end
+        when :javascript
+          attributes.each do |type, type_value|
+            new_hash[:javascript][:style] << style
+          end
+        end
+      end
+    end
+  end
+  new_hash
 end
